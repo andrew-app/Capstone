575208b69d66a4c156b925c3e61b308b761e773a
andrewapp <andrew.appuhamy@gmail.com>
2022-08-09 12:53:49 +1000

    Basic 3 channel image

--- three_channel/src/main.rs
+++ three_channel/src/main.rs
@@ -1,12 +1,27 @@
-use image::GenericImageView;
-
+use image::{RgbImage, Rgb};
 fn main() {
 
-    let R = image::open("/home/andrewapp/Documents/Capstone/three_channel/assets/Image_050_6.jpg").unwrap();
-    let G = image::open("/home/andrewapp/Documents/Capstone/three_channel/assets/Image_050_3.jpg").unwrap();
-    let B = image::open("/home/andrewapp/Documents/Capstone/three_channel/assets/Image_050_1.jpg").unwrap();
+    let r = image::open("./assets/Image_050_6.jpg").unwrap_or_default();
+    let g = image::open("./assets/Image_050_3.jpg").unwrap_or_default();
+    let b = image::open("./assets/Image_050_1.jpg").unwrap_or_default();
+
+    let r = r.as_bytes();
+    let g = g.as_bytes();
+    let b = b.as_bytes();
 
     
-    let img = [R,G,B];
-    image::save_buffer("test.jpg", img, 500, 500, image::ColorType::Rgb8).unwrap()
+
+    let mut img = RgbImage::new(500, 500);
+    let mut iter = 0;
+    for x in 0..500 {
+        for y in 0..500 {
+            img.put_pixel(y, x, Rgb([r[iter],g[iter],b[iter]]));
+            iter += 1;
+        }
+    }
+
+    
+    let format = image::ColorType::Rgb8;
+    image::save_buffer("./output/Test_Img.jpg", &img, 500, 500, format).unwrap_or_default();
+    
 }
